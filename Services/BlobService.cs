16a51e51a40730e56d591ed8c52a339eb462084c
Ethan James Donaldson <ethanali33@gmail.com>
2024-08-30 22:15:13 +0200

    Add project files.

---
+++ Services/BlobService.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace POE_CLDV2B.Services
+{
+    public class BlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobService(IConfiguration configuration)
+        {
+            _blobServiceClient = new BlobServiceClient(configuration["AzureStorage:ConnectionString"]);
+        }
+
+        public async Task UploadBlobAsync(string containerName, string blobName, Stream content)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+            var blobClient = containerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(content, true);
+        }
+    }
+}
